77c53972a69202a10e9327e6b408c286cbb327ec
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2025-02-18 20:27:52 +0600

    oder api with class params

--- fastAPI_openai/app/main.py
+++ fastAPI_openai/app/main.py
@@ -5,6 +5,11 @@ from pydantic import BaseModel
 # initialize FastAPI
 app = FastAPI()
 
+# create class for Order parameters
+class Order(BaseModel):
+    units: int
+    product: str
+
 @app.get("/start")
 async def start():
     return {"message": "Starting the FastAPI server"}
@@ -12,3 +17,8 @@ async def start():
 @app.get("/hello")
 def hello(name: str = "Pranto"):
     return {"message": f"Hello {name}"}
+
+@app.post("/order")
+def order(orderParams: Order):
+    return {"message": f"Order for {orderParams.units} units of {orderParams.product} is placed"}
+
