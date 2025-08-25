33f94b03627f2ca964ec8f0fbecf14cb586f0fb7
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2025-08-25 16:26:04 +0600

    making and serving ml model with fastapi

--- Concepts/path_parameter.py
+++ Concepts/path_parameter.py
@@ -15,7 +15,9 @@ def load_data():
 # view specific patient with a patient_id in the path
 @app.get('/patient/{patient_id}')
 # patient_id is a path parameter
-def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+def view_patient(
+    patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')
+    ):
     # load all the patients
     data = load_data()
     # check if the patient_id exists
